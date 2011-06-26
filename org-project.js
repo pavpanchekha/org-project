450f23d5bbe95ecdeb6660df4cb1dece4a7e731b
Pavel Panchekha <pavpanchekha@gmail.com>
2011-06-26 02:59:34 -0700

    New global org-project.org org-mode file added

--- org-project.js
+++ org-project.js
@@ -1,5 +1,8 @@
+
 $(function () {
-    pageSection();
+    if (!$("#pages").length) {
+        pageSection();
+    }
     
     if (window.location.hash) {
         $("#pages li a").filter(function () {
@@ -12,15 +15,23 @@ $(function () {
     if (Hyphenator) {
         Hyphenator.run();
     }
-    //$(".section h1").hide();
+
+    $(".outline-2 h2").css({
+        "font-size": 0,
+        "margin": 0,
+        "padding": 0,
+        "height": 0,
+    });
 });
 
 function pageSection() {
     var sections = new Array();
+    console.log(".outline-2");
     $(".outline-2").each(function (i) {
-        var text = $(this).find("h2").text()
+        var text = $(this).find("h2").text();
         if (text) {
             text = text.trim().replace(/\[[\d/%]+\]/, "");
+            text = text.trim().replace(/^[\d\.]\s+/, "");
             sections.push({text: text, id: i});
         }
     });
